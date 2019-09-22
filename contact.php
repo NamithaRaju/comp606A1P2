2c52f9790414bdc40d00fc39bfe9e3b2a5487188
namithaodackal1996@gmail.com <namithaodackal1996@gmail.com>
2019-09-23 00:16:43 +1200

    contact page
    this page allows the customer to access the contact of admins when they have any queries by email them.

---
+++ contact.php
@@ -0,0 +1,84 @@
+<?php
+	session_start();
+	require_once('config.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+    body {
+	font-size: 120%;
+	background-image: url("sports-massage.jpg");
+	background-repeat: no-repeat;
+    background-size: cover;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: right;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.table{
+  border-radius : 10px;
+	background-color: white;
+	border: 3px solid #133783;
+	width: 376px;
+	margin: 0 auto;
+}
+.content {
+  width: 80%;
+  margin: 0px auto;
+  text-align: center;
+  font-size: 40px;
+  padding: 20px;
+  border: 3px solid blue;
+  border-top: none;
+  background: white;
+  border-radius: 0px 0px 10px 10px;
+}
+</style>
+</head>
+<body>
+
+<div class="topnav">
+  <a class="active" href="contact.php">Contact Us</a>
+  <a href="about.php">About Me</a>
+  <a href="cancellation.php">Cancel Appoinment</a>
+  <a href="appointment.php">Make Appoinment</a>
+  <a href="login.php">Sign In</a>
+  <a href="home.php">Home</a>
+</div>
+<div>
+<h1>Welcome to Sports Massage Hamilton<h1>
+</div>
+<div>
+
+    <h3>Email us: ann@hotmail.co.nz<br> nam@gmail.com</h3>
+
+</div>
+</body>
+</html>
+
