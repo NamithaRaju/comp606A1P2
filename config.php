43b07ff0652f22823fa5b544add64bfb0700a6cb
namithaodackal1996@gmail.com <namithaodackal1996@gmail.com>
2019-09-23 21:17:17 +1200

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
- 
+ // initiates variables
 $mysql_host='localhost';
 $mysql_user='root';
 $mysql_password='';
 $database='booking';
-
+// connects to the database
 $db=mysqli_connect($mysql_host,$mysql_user,$mysql_password,$database);
 if ($db == false){
-    header("location: sitedown.php");
+    header("location: sitedown.php");//redirects to sitedown page
   }
 
 ?>
